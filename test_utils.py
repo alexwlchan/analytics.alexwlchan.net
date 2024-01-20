5e5c1634d8d865f622cd972bd0dee896cd84a7ea
Alex Chan <alex@alexwlchan.net>
2024-01-20 00:22:24 +0000

    Add an actual test using the code

--- test_utils.py
+++ test_utils.py
@@ -1,2 +1,14 @@
-def test_truth() -> None:
-    assert True
+import pytest
+
+from utils import get_country_iso_code
+
+
+@pytest.mark.parametrize(
+    ["ip_address", "country_code"],
+    [
+        ("52.85.118.55", "US"),
+        ("127.0.0.1", None),
+    ],
+)
+def test_get_country_iso_code(ip_address, country_code):
+    assert get_country_iso_code(ip_address) == country_code
