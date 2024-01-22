1ec2b393973605b37157eb5f4a34f28ea6c109c5
Alex Chan <alex@alexwlchan.net>
2024-01-22 20:43:17 +0000

    get the world map and other bits working

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import datetime
 import json
 
 from flask import Flask, render_template
+import pycountry
 
 from utils import get_database
 
@@ -12,9 +13,8 @@ app = Flask(__name__)
 db = get_database(path="requests.sqlite")
 
 
-@app.route("/")
-def index():
-    by_date = db.query(
+def count_requests_by_day():
+    return db.query(
         """
         select
           substring(date, 0, 11) as day,
@@ -32,7 +32,9 @@ def index():
     """
     )
 
-    unique_users = db.query(
+
+def count_unique_visitors():
+    return db.query(
         """
         select
           substring(date, 0, 11) as day,
@@ -49,7 +51,36 @@ def index():
     """
     )
 
-    top_pages = db.query(
+
+def count_visitors_by_country():
+    cursor = db.query(
+        """
+        select
+          country,
+          count(*) as count
+        from
+          events
+        where
+          date >= :date
+          and country != ''
+          and host != 'localhost' and host not like '%--alexwlchan.netlify.app'
+        group by
+          country
+        order by
+          count desc
+        """,
+        {
+            "date": (datetime.date.today() - datetime.timedelta(days=29)).strftime(
+                "%Y-%m-%d"
+            )
+        },
+    )
+
+    return collections.Counter({row['country']: row['count'] for row in cursor})
+
+
+def find_popular_pages():
+    return db.query(
         """
         select
           *,
@@ -73,7 +104,9 @@ def index():
         },
     )
 
-    missing_pages = db.query(
+
+def find_missing_pages():
+    return db.query(
         """
         select
           path,
@@ -98,6 +131,8 @@ def index():
         },
     )
 
+
+def find_grouped_referrers():
     referrers_by_page = db.query(
         """
         select
@@ -128,12 +163,77 @@ def index():
 
     grouped_referrers = sorted(grouped_referrers.items(), key=lambda kv: sum(kv[1].values()), reverse=True)
 
+    return grouped_referrers
+
+
+def get_flag_emoji(country_id: str) -> str:
+    code_point_start = ord("🇦") - ord("A")
+    assert code_point_start == 127397
+
+    code_points = [code_point_start + ord(char) for char in country_id]
+    return "".join(chr(cp) for cp in code_points)
+
+
+def get_country_name(country_id: str) -> str:
+    if country_id == 'US':
+        return 'USA'
+
+    if country_id == 'GB':
+        return 'UK'
+
+    if country_id == 'RU':
+        return 'Russia'
+
+    c = pycountry.countries.get(alpha_2=country_id)
+
+    if c is not None:
+        return c.name
+    else:
+        return country_id
+
+
+def get_hex_color_between(hex1, hex2, proportion):
+    r1, g1, b1 = int(hex1[1:3], 16), int(hex1[3:5], 16), int(hex1[5:7], 16)
+    r2, g2, b2 = int(hex2[1:3], 16), int(hex2[3:5], 16), int(hex2[5:7], 16)
+
+    r_new = int(r1 + (r2 - r1) * proportion)
+    g_new = int(g1 + (g2 - g1) * proportion)
+    b_new = int(b1 + (b2 - b1) * proportion)
+
+    return f'#%02x%02x%02x' % (r_new, g_new, b_new)
+
+
+app.jinja_env.filters['flag_emoji'] = get_flag_emoji
+app.jinja_env.filters['country_name'] = get_country_name
+app.jinja_env.filters['interpolate_color'] = get_hex_color_between
+
+
+@app.route("/")
+def index():
+    by_date = count_requests_by_day()
+
+    unique_users = count_unique_visitors()
+
+    top_pages = find_popular_pages()
+
+    missing_pages = find_missing_pages()
+
+    grouped_referrers = find_grouped_referrers()
+
+    visitors_by_country = count_visitors_by_country()
+
+    country_names = {
+        country: get_country_name(country)
+        for country in visitors_by_country
+    }
+
     return render_template(
         "dashboard.html",
         by_date=sorted(by_date, key=lambda row: row["day"]),
         unique_users=sorted(unique_users, key=lambda row: row["day"]),
         top_pages=list(top_pages),
         missing_pages=list(missing_pages),
-        referrers_by_page=referrers_by_page,
         grouped_referrers=grouped_referrers,
+        visitors_by_country=count_visitors_by_country(),
+        country_names=country_names,
     )
