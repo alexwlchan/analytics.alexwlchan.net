2c44a19230ebe9c5a924fd4ae9f455f0484b7e4d
Alex Chan <alex@alexwlchan.net>
2024-03-05 20:36:11 +0000

    Run black for autoformatting

--- app.py
+++ app.py
@@ -358,7 +358,9 @@ def get_netlify_bandwidth_usage():
 
     data = resp.json()
 
-    data["period_start_date"] = datetime.datetime.fromisoformat(data["period_start_date"])
+    data["period_start_date"] = datetime.datetime.fromisoformat(
+        data["period_start_date"]
+    )
     data["period_end_date"] = datetime.datetime.fromisoformat(data["period_end_date"])
 
     return data
@@ -423,7 +425,9 @@ app.jinja_env.filters["interpolate_color"] = get_hex_color_between
 app.jinja_env.filters["naturalsize"] = humanize.naturalsize
 app.jinja_env.globals["circular_arc"] = get_circular_arc_path_command
 
-app.jinja_env.globals.update(now=lambda: datetime.datetime.now(tz=datetime.timezone.utc))
+app.jinja_env.globals.update(
+    now=lambda: datetime.datetime.now(tz=datetime.timezone.utc)
+)
 
 
 @app.template_filter("prettydate")
