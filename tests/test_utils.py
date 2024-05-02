f3c977e189c53fd688dc75da1b78c7482741332a
Alex Chan <alex@alexwlchan.net>
2024-05-02 08:43:36 +0100

    Handle a country ID which is `None`
    There are some rows in the existing database where I wasn't able to
    guess a country code, so the database value is `None`.  This throws
    a LookupError when you ask pycountry what name it is, so handle that
    separately.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from analytics.utils import get_country_iso_code
+from analytics.utils import get_country_iso_code, get_country_name
 
 
 @pytest.mark.parametrize(
@@ -10,3 +10,18 @@ def test_get_country_iso_code(
     maxmind_database: None, ip_address: str, country_code: str | None
 ) -> None:
     assert get_country_iso_code(ip_address) == country_code
+
+
+@pytest.mark.parametrize(
+    ["country_id", "country_name"],
+    [
+        ("US", "USA"),
+        ("GB", "UK"),
+        ("RU", "Russia"),
+        ("CN", "China"),
+        ("XK", "XK"),
+        (None, "<unknown>"),
+    ],
+)
+def test_get_country_name(country_id: str | None, country_name: str) -> None:
+    assert get_country_name(country_id) == country_name
