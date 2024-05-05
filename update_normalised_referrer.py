aa89f38d0cd24f540f7af8d9c4af551f9c9b4a07
Alex Chan <alex@alexwlchan.net>
2024-05-05 22:07:21 +0100

    Add caching to normalise referrer; only get interesting rows

--- update_normalised_referrer.py
+++ update_normalised_referrer.py
@@ -2,19 +2,28 @@
 Update the database with the latest definitions of get_normalised_referrer().
 """
 
+import functools
 import json
 
 import tqdm
 
-from analytics.referrers import get_normalised_referrer
+from analytics.referrers import get_normalised_referrer, QueryParams
 from analytics.utils import get_database
 
 
+@functools.cache
+def parse_query(qs: str) -> QueryParams:
+    return tuple(tuple(q) for q in json.loads(qs))
+
+
 def get_events_to_upsert(db):
-    for row in tqdm.tqdm(db["events"].rows, total=db["events"].count):
+    cursor = db["events"].rows_where("referrer != '' or query != '[]'")
+    total = db["events"].count_where("referrer != '' or query != '[]'")
+
+    for row in tqdm.tqdm(cursor, total=total):
         normalised_referrer = get_normalised_referrer(
             referrer=row["referrer"],
-            query=tuple(tuple(q) for q in json.loads(row["query"])),
+            query=parse_query(row["query"]),
         )
 
         if normalised_referrer != row["normalised_referrer"]:
