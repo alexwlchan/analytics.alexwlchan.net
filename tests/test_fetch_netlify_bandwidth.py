2a0f00816f2f99bbe41a71cf4012446cfae4a658
Alex Chan <alex@alexwlchan.net>
2024-08-24 17:37:47 +0100

    Remove some duplicate `tmp_working_dir` fixtures
    This fixture is already defined in `conftest.py`; we don't need to repeat
    it in these two files.

--- tests/test_fetch_netlify_bandwidth.py
+++ tests/test_fetch_netlify_bandwidth.py
@@ -1,17 +1,10 @@
 import datetime
 import json
-import os
 import pathlib
 
 import pytest
 
 from analytics.fetch_netlify_bandwidth import fetch_netlify_bandwidth_usage
-
-
-@pytest.fixture
-def tmp_working_dir(tmp_path: pathlib.Path) -> pathlib.Path:
-    os.chdir(tmp_path)
-    return tmp_path
 
 
 @pytest.mark.vcr()
