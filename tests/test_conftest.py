482e69f1897e0b03a2ebaa5e9fb9171049be7a1a
Alex Chan <alex@alexwlchan.net>
2024-08-24 18:00:41 +0100

    Add docstrings to `test_conftest.py`

--- tests/test_conftest.py
+++ tests/test_conftest.py
@@ -1,3 +1,7 @@
+"""
+Tests for the pytest fixtures defined in ``conftest.py``.
+"""
+
 import os
 import re
 
@@ -6,6 +10,9 @@ import pytest
 
 @pytest.mark.parametrize("cassette_name", os.listdir("tests/cassettes"))
 def test_no_netlify_token_in_cassettes(cassette_name: str) -> None:
+    """
+    There's nothing that looks like a Netlify API token in the test cassettes.
+    """
     with open(os.path.join("tests/cassettes", cassette_name)) as in_file:
         cassette_contents = in_file.read()
 
