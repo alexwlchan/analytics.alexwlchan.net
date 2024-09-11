029b048793f58f4aa16a7e504d11e84b966ba17a
Alex Chan <alex@alexwlchan.net>
2024-09-11 08:10:41 +0100

    Start adding a list of referrers to this repo

---
+++ referrers/referrer_metadata.js
@@ -0,0 +1,12 @@
+const referrers = [
+  {
+    "url": "https://www.bryanbraun.com/2023/09/11/links-8/",
+    "title": "Links #8",
+    "author": "Bryan Braun",
+    "date_published": "2023-09-11",
+    "comments": null,
+    "links_to": "https://alexwlchan.net/2022/screenshots/",
+    "archive_path": "archive/bryanbraun.com/links-8.webarchive",
+    "archive_saved_at": "2024-09-11 08:09:19 +0100"
+  }
+];
