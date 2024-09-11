caf43b3ad4f77291f7c78a223ae64511ed80a4b3
Alex Chan <alex@alexwlchan.net>
2024-09-11 08:28:10 +0100

    Add another referrer

--- referrers/referrer_metadata.js
+++ referrers/referrer_metadata.js
@@ -4,7 +4,6 @@ const referrers = [
     "title": "Links #8",
     "author": "Bryan Braun",
     "date_published": "2023-09-11",
-    "comments": null,
     "links_to": "https://alexwlchan.net/2022/screenshots/",
     "archive_path": "archive/bryanbraun.com/links-8.webarchive",
     "archive_saved_at": "2024-09-11 08:09:19 +0100"
@@ -14,7 +13,6 @@ const referrers = [
     "title": "Un PDF de dimensiones colosales",
     "author": "Alvy",
     "date_published": "2024-03-31",
-    "comments": null,
     "links_to": "https://alexwlchan.net/2024/big-pdf/",
     "archive_path": "archive/microsiervos.com/pdf-dimensiones-colosales.webarchive",
     "archive_saved_at": "2024-09-11 08:15:17 +0100",
@@ -25,9 +23,17 @@ const referrers = [
     "title": "Lazy Reading for 2019/05/19",
     "author": "Justin Sherrill",
     "date_published": "2019-05-19",
-    "comments": null,
     "links_to": "https://alexwlchan.net/2019/05/falsehoods-programmers-believe-about-unix-time/",
     "archive_path": "archive/dragonflydigest.com/lazy-reading-for-2019-05-19.webarchive",
     "archive_saved_at": "2024-09-11 08:21:51 +0100"
+  },
+  {
+    "url": "https://briefdose.substack.com/p/4-gonorrhea-snap-and-22-trillion",
+    "title": "#4: gonorrhea, Snap and $2.2 trillion",
+    "author": "Briefdose",
+    "date_published": "2024-02-05",
+    "links_to": "https://alexwlchan.net/2024/big-pdf/",
+    "archive_path": "archive/briefdose.substack.com/4-gonorrhea-snap-and-22-trillion.webarchive",
+    "archive_saved_at": "2024-09-11 08:27:57 +0100"
   }
 ];
