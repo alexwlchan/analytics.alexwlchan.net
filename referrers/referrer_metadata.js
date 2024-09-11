13a2b0df33b3ff5c0457aa15366410d8670656ee
Alex Chan <alex@alexwlchan.net>
2024-09-11 08:16:12 +0100

    Add a second referrer

--- referrers/referrer_metadata.js
+++ referrers/referrer_metadata.js
@@ -8,5 +8,16 @@ const referrers = [
     "links_to": "https://alexwlchan.net/2022/screenshots/",
     "archive_path": "archive/bryanbraun.com/links-8.webarchive",
     "archive_saved_at": "2024-09-11 08:09:19 +0100"
+  },
+  {
+    "url": "https://www.microsiervos.com/archivo/curiosidades/pdf-dimensiones-colosales.html",
+    "title": "Un PDF de dimensiones colosales",
+    "author": "Alvy",
+    "date_published": "2024-03-31",
+    "comments": null,
+    "links_to": "https://alexwlchan.net/2024/big-pdf/",
+    "archive_path": "archive/microsiervos.com/pdf-dimensiones-colosales.webarchive",
+    "archive_saved_at": "2024-09-11 08:15:17 +0100",
+    "language": "es"
   }
 ];
