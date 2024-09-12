3d3afc6fee0f50a51dd10fdfcf4ada2e673f4b30
Alex Chan <alex@alexwlchan.net>
2024-09-12 07:03:58 +0100

    Add a few more referrers, allow quotes

--- referrers/referrer_metadata.js
+++ referrers/referrer_metadata.js
@@ -6,7 +6,8 @@ const referrers = [
     "date_published": "2023-09-11",
     "links_to": "https://alexwlchan.net/2022/screenshots/",
     "archive_path": "archive/bryanbraun.com/links-8.webarchive",
-    "archive_saved_at": "2024-09-11 08:09:19 +0100"
+    "archive_saved_at": "2024-09-11 08:09:19 +0100",
+    "quote": "An argument for using screenshots for digital preservation. I’ve kept screenshots for many of the websites I’ve made (see my bryanbraun.com redesign post for one example) and this post inspired me to go back and collect some more (especially for past client projects). It’s best to collect these soon… the longer you wait the more difficult it’ll be to capture them."
   },
   {
     "url": "https://www.microsiervos.com/archivo/curiosidades/pdf-dimensiones-colosales.html",
@@ -16,7 +17,8 @@ const referrers = [
     "links_to": "https://alexwlchan.net/2024/big-pdf/",
     "archive_path": "archive/microsiervos.com/pdf-dimensiones-colosales.webarchive",
     "archive_saved_at": "2024-09-11 08:15:17 +0100",
-    "language": "es"
+    "language": "es",
+    "quote": "Pese a esto la mayor parte de los programas no funcionan más allá de 200 × 200 pulgadas y hay quien ha encontrado trucos para superar este tamaño y crear PDFs más grandes que el Universo conocido."
   },
   {
     "url": "https://www.dragonflydigest.com/2019/05/19/lazy-reading-for-2019-05-19/",
@@ -44,7 +46,8 @@ const referrers = [
     "comment": "Includes a link to my Overcast piece, which accounted for a small spike in traffic. :)",
     "links_to": "http://alexwlchan.net/2014/07/overcast/",
     "archive_path": "archive/mjtsai.com/overcast-rejected-for-listing-competing-podcast-apps.webarchive",
-    "archive_saved_at": "2024-09-11 08:30:55 +0100"
+    "archive_saved_at": "2024-09-11 08:30:55 +0100",
+    "quote": "This is what the list in question looked like. I think it was genuinely useful."
   },
   {
     "url": "https://forum.dji.com/thread-31953-1-2.html",
@@ -52,6 +55,17 @@ const referrers = [
     "date_published": "2015-09-23",
     "links_to": "http://alexwlchan.net/2013/08/google-maps/",
     "archive_path": "archive/forum.dji.com/thread-31953-1-2.webarchive",
-    "archive_saved_at": "2024-09-11 08:39:46 +0100"
+    "archive_saved_at": "2024-09-11 08:39:46 +0100",
+    "quote": "I use Google Maps often for planning flights and driving trips, but detest that annoying search box in the upper left corner of the screen that keeps expanding and contracting depending on my search actions.  I found a solution on the 'net that solves the problem by toggling on/off that search box with one click on my Windows PC and Linux laptop (I use Firefox browser on both). Go to this pageand drag the “Toggle Google Maps” thing to your browser's bookmarks tool bar ( I suppose it'll work in Internet Explorer too?).  Now when you're on a map all you have to do is click it and the search box disappears---click it again and it reappears.   Google Maps no longer makes me crazy."
+  },
+  {
+    "url": "https://unwrapping.tumblr.com/post/99176216337/find-untagged-tumblr-posts",
+    "title": "Find Untagged Tumblr Posts",
+    "author": "Mykl Novak",
+    "date_published": "2014-10-04",
+    "links_to": "http://finduntaggedtumblrposts.com/",
+    "archive_path": "archive/unwrapping.tumblr.com/find-untagged-tumblr-posts.webarchive",
+    "archive_saved_at": "2024-09-12 07:01:01 +0100",
+    "quote": "Alex Chan (alexwlchan) created finduntaggedtumblrposts.com to track down your Tumblr posts without any tags. I’ve added this resource to my blogkeeping checklist."
   }
 ];
